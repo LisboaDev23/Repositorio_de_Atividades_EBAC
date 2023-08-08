17bb4734272a6ce91536b4466a2e33cea73da69b
Gabriel Pereira Lisboa <134669989+LisboaDev23@users.noreply.github.com>
2023-08-08 15:10:33 -0300

    Commit para enviar tarefa

--- src/ResultadoNota.java
+++ src/ResultadoNota.java
@@ -11,32 +11,16 @@ public class ResultadoNota {
         aluno.setNome(scann.nextLine());
         System.out.println("Digite sua primeira nota: ");
         int nota1 = scann.nextInt();
-        if (nota1 > 10 || nota1 < 0) {
-            System.out.println("Não é possível inserir essa nota!");
-        } else {
             System.out.println("Sua primeira nota foi : " +nota1);
-        }
         System.out.println("Digite sua segunda nota: ");
         int nota2 = scann.nextInt();
-        if (nota2 > 10 || nota2 < 0) {
-            System.out.println("Não é possível inserir essa nota!");
-        } else {
             System.out.println("Sua primeira nota foi : "+nota1+ ", Sua segunda nota foi : " +nota2  );
-        }
         System.out.println("Digite sua terceira nota: ");
         int nota3 = scann.nextInt();
-        if (nota3 > 10 || nota3 < 0) {
-            System.out.println("Não é possível inserir essa nota!");
-        } else {
             System.out.println("Sua primeira nota foi : "+nota1+ ", Sua segunda nota foi : " +nota2+", Sua terceira nota foi : " +nota3);
-        }
         System.out.println("Digite sua última nota: ");
         int nota4 = scann.nextInt();
-        if (nota4 > 10 || nota4 < 0) {
-            System.out.println("Não é possível inserir essa nota!");
-        } else {
             System.out.println("Sua primeira nota foi : "+nota1+ ", Sua segunda nota foi : " +nota2+", Sua terceira nota foi : " +nota3+", Sua quarta nota foi : " +nota4);
-        }
         int notaResultado = (nota1+nota2+nota3+nota4)/4  ;
         System.out.println("Sua média foi: " +notaResultado);
         if (notaResultado>=7) {
